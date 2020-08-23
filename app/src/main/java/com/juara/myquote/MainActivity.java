7b577b235e7f8d625dec3de4ba1ca90aab557620
samga <samgalaxy111111@gmail.com>
2020-08-23 16:41:07 +0700

    Koneksi ke Web API & Parsing Json , Satu JSONObject

--- app/src/main/java/com/juara/myquote/MainActivity.java
+++ app/src/main/java/com/juara/myquote/MainActivity.java
@@ -3,12 +3,83 @@ package com.juara.myquote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvQuote, tvAuthor;
+    private ProgressBar progressBar;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvQuote = findViewById(R.id.tvQuote);
+        tvAuthor = findViewById(R.id.tvAuthor);
+        progressBar = findViewById(R.id.progressBar);
+
+        getRandomQuote();
+    }
+
+    private void getRandomQuote(){
+        progressBar.setVisibility(View.VISIBLE);
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "https://programming-quotes-api.herokuapp.com/quotes/random";
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                progressBar.setVisibility(View.INVISIBLE);
+
+                String result = new String(responseBody);
+                Log.d(TAG, result);
+                try {
+                    JSONObject responseObject = new JSONObject(result);
+
+                    String quote = responseObject.getString("en");
+                    String author = responseObject.getString("author");
+
+                    tvQuote.setText(quote);
+                    tvAuthor.setText(author);
+                } catch (Exception e){
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                progressBar.setVisibility(View.INVISIBLE);
+
+                String errorMessage;
+                switch (statusCode){
+                    case 401:
+                        errorMessage = statusCode+" : Bad Request";
+                        break;
+                    case 403:
+                        errorMessage = statusCode+" : Forbidden";
+                        break;
+                    case 404:
+                        errorMessage = statusCode+" : NotFound";
+                        break;
+                    default:
+                        errorMessage = statusCode+" : "+error.getMessage();
+                        break;
+                }
+                Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
